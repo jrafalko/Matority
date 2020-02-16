7803414ca9727307a956aa8806bdb21642b71e1a
Janusz Rafalko <j.rafalko@mini.pw.edu.pl>
2020-02-16 10:49:15 +0100

    Sprawdzenie pełnoletności

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.print("Urodzony 16.02.2002 - ");
+        if (majority(16, 2, 2002))
+            System.out.println("pełnoletni");
+        else
+            System.out.println("niepełnoletni");
+
+
     }
+
+    public static boolean majority(int day, int month, int year) {
+        LocalDate dateOfBirth, now;
+        dateOfBirth = LocalDate.of(year, month, day);
+        now = LocalDate.now();
+        Period p = Period.between(dateOfBirth, now);
+
+        if(p.getYears() >= 18) return true;
+        return false;
+    }
+
 }
